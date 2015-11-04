8f915314a7c6c7e0c6b9bdef855cedb711cbfee8
manjunath <manju.ole23@GMAIL.COM>
2015-11-04 16:58:07 +0530

    Added New file

---
+++ src/test/java/com/pp/pages/Login_HomePage.java
@@ -0,0 +1,13 @@
+package com.pp.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.pp.util.BasePageObject;
+
+public class Login_HomePage extends BasePageObject
+{
+	public Login_HomePage(WebDriver driver)
+	{
+		super(driver);
+	}
+}
