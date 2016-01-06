7da22d76663971436b94024ede2a88db2a35f872
manjunath <manju.ole23@GMAIL.COM>
2016-01-06 11:33:26 +0530

    Done Changes

---
+++ src/test/java/com/pp/pages/RequestSubmitted1Page.java
@@ -0,0 +1,16 @@
+package com.pp.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.pp.util.BasePageObject;
+
+	public class RequestSubmitted1Page extends BasePageObject
+	{
+
+		public RequestSubmitted1Page(WebDriver driver) {
+			super(driver);
+		}
+
+		
+	}
+
